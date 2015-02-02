7579a42458039f67ffda3c5959c1935b15ae0e9a
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2015-02-02 10:06:42 -0800

    Add a directory walker that passes files to a tar header which is then converted json. All the files are collected and written to one file.

---
+++ manifest.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"archive/tar"
+	"flag"
+	"os"
+	"path/filepath"
+	"log"
+	"encoding/json"
+	"io/ioutil"
+)
+
+var manifest []*tar.Header
+
+func visit(path string, f os.FileInfo, err error) error {
+	log.Printf("Visited: %s\n", path)
+	h, _ := tar.FileInfoHeader(f, path)
+	if err != nil {
+        log.Printf("Error: %s", err)
+		return err;
+	}
+	//log.Println(string(b))
+	manifest = append(manifest, h)
+	return nil
+}
+
+func main() {
+	flag.Parse()
+	root := flag.Arg(0)
+	if root == "" {
+		root = "."
+	} 
+	err := filepath.Walk(root, visit)
+	if err != nil {
+		log.Printf("filepath.Walk() returned %v\n", err)
+	}
+	m, err := json.Marshal(manifest)
+	if err != nil {
+        log.Printf("Error: %s", err)
+		panic(err)
+	}
+	log.Printf("Manifest is: %s", m)
+	ioutil.WriteFile("manifest.json", []byte(m), 0x755)
+}
